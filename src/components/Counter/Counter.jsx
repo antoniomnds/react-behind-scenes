abdc8558c2f36e955e0f4aa5a9a6bf03f55e6bdd
Antonio Santos <antoniomndsantos@gmail.com>
2024-12-13 12:44:35 +0000

    Check if counter value is prime only when the initial value changes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -29,7 +29,9 @@ function isPrime(number) {
 
 export default function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+
+  // initialCount only changes when the set button is clicked. Re-executions can be avoided when the counter changes
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
   const [counter, setCounter] = useState(initialCount);
 
